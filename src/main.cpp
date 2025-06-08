f7fc89dff429a4102ea80f85a272ea238da84dc9
ReverseRasterization <zach.morrato@outlook.com>
2025-06-08 17:00:53 -0600

    Zoom, Textures Prepared

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,20 @@
-// TOOD: Zoom Feature
-
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 #include <SFML/Graphics.hpp>
 #include "map.h"
 
+sf::Vector2f clampVector(sf::Vector2f vector, sf::Vector2f min_vector, sf::Vector2f max_vector);
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({800,800}), "My window");
+    sf::RenderWindow window(sf::VideoMode({800,800}), "My window", sf::Style::Titlebar | sf::Style::Close);
     sf::View camera;
 
     camera.setCenter({400,400});
     camera.setSize({800,800});
 
     Map map;
-    sf::RectangleShape indicator({50.f, 50.f});
-    indicator.setPosition({750, 750});
-    indicator.setFillColor(sf::Color::Red);
-
     if (map.load(10, 10, 30))
     {   
         std::cout << "Loaded!\n";
@@ -25,11 +23,18 @@ int main()
         return -1; 
     }
 
+    // Variables for camera movement
     bool subscribed;
     sf::Vector2i initialMousePos;
     sf::Vector2f initialWorldPos;
-   
 
+    // Variables for zoom
+    sf::Vector2f baseSize = camera.getSize();
+    sf::Vector2f minSize = baseSize/2.f; // how much the user can zoom in
+    sf::Vector2f maxSize = baseSize*2.f; // how much the user can zoom out
+    float zoomStep = 0.15f;
+    float zoomFactor = 1.f;
+   
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -39,14 +44,17 @@ int main()
                 window.close();
             }
 
+            // Camera Movement
             if (event->is<sf::Event::MouseMoved>() && subscribed) 
             {
                 auto mousePos = sf::Mouse::getPosition(window);
 
-                float xDiff = mousePos.x - initialMousePos.x;
-                float yDiff = mousePos.y - initialMousePos.y;
+                float xDiff = (mousePos.x - initialMousePos.x) * zoomFactor;
+                float yDiff = (mousePos.y - initialMousePos.y) * zoomFactor;
 
-                camera.setCenter({initialWorldPos.x-xDiff, initialWorldPos.y - yDiff});
+                sf::Vector2f delta = {xDiff, yDiff};
+
+                camera.setCenter(initialWorldPos - delta);
             }
 
             if (event->is<sf::Event::MouseButtonPressed>())
@@ -60,17 +68,38 @@ int main()
             {
                 subscribed = false;
             }
+
+            // Zoom
+            if (const auto* mouseWheelScrolled = event->getIf<sf::Event::MouseWheelScrolled>())
+            {
+                if(mouseWheelScrolled->wheel == sf::Mouse::Wheel::Vertical)
+                {
+                    sf::Vector2f cameraSize = camera.getSize();
+
+                    if(mouseWheelScrolled->delta == 1) { // up
+                        camera.setSize(clampVector(cameraSize*(1.f-zoomStep), minSize, maxSize));
+                    }else if (mouseWheelScrolled->delta == -1) { // down
+                        camera.setSize(clampVector(cameraSize*(1.f+zoomStep), minSize, maxSize));
+                    }
+
+                    zoomFactor = cameraSize.x / baseSize.x;
+                }
+            }
         }
 
         window.clear();
-
-        window.draw(indicator);
-
         window.setView(camera);
         window.draw(map);
-        
         window.display();
     }
 
     return 0;
+}
+
+sf::Vector2f clampVector(sf::Vector2f vector, sf::Vector2f min_vector, sf::Vector2f max_vector)
+{
+    float x = std::clamp(vector.x, min_vector.x, max_vector.x);
+    float y = std::clamp(vector.y, min_vector.y, max_vector.y);
+
+    return {x,y};
 }
